863979055364ed4d6df79140a3f9b5f967efbe58
jaymepsantos <63884111+jaymepsantos@users.noreply.github.com>
2025-04-12 08:01:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const terminalTextElement = document.getElementById('terminal-text');
 
 
-
+const phrases = [
+    "Acorde, Neo...",
+    "A Matrix achou você...",
+    "Siga o coelho branco.",
+    "O que é real? Como você define o real?",
+    "Você já teve um sonho, Neo, do qual tinha tanta certeza que era real?",
+    "Infelizmente, ninguém pode ser informado sobre o que é a Matrix. Você tem que ver por si mesmo.",
+    "Estou oferecendo a verdade. Nada mais.",
+    "Bem-vindo ao deserto do real.",
+    "Esta é a sua última chance. Depois disso, não há volta.",
+    "Se tomar a pílula azul, a história acaba. Você acorda na sua cama e acredita no que quiser.",
+    "Se tomar a pílula vermelha, fica no País das Maravilhas, e eu te mostrarei até onde vai a toca do coelho.",
+    "Acorde, Neo...",
+    "A Matrix achou você...",
+    "Siga o coelho branco.",
+    "Humanos são um vírus. Vocês são a praga deste planeta. E nós... somos a cura.",
+    "Sr. Anderson...",
+    "Você ouve isso, Sr. Anderson? Isso é o som da inevitabilidade.",
+    "Você está dizendo que eu posso desviar de balas?",
+    "Eu sei kung fu.",
+    "Por que meus olhos doem?",
+    "Porque você nunca os usou antes."
+];
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
